f71c039f0e65b3dadf9c247ad9b0bec20ad94637
Guillermo Priego Sierra <Gpriego@outlook.com>
2020-11-21 15:51:47 +0100

    Quitar imports sin usar

--- React-kabel-capacitacion/ClientApp/src/App.js
+++ React-kabel-capacitacion/ClientApp/src/App.js
@@ -1,5 +1,4 @@
 import * as React from 'react'
-import { ReactQueryDevtools } from "react-query-devtools";
 import { Route, Switch } from 'react-router-dom';
 import FeedForm from './components/feeds/FeedForm';
 import FeedList from './components/feeds/FeedList';
